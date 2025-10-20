d38c111c5063d8b6062c24da336f0201d0c5bc90
Thiago Carvalho <thiago@MacBook-Air-de-Thiago.local>
2025-10-20 19:09:34 -0300

    Get token from discord via env file

--- bot/discord_bot.py
+++ bot/discord_bot.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
 import yt_dlp as youtube_dl
+import os
 
-TOKEN = ""
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
